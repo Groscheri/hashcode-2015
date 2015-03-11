3c9e881a6f8d4fdff1364b9efddf2509e9a1de55
Leonard <leonard.allain-launay@insa-lyon.fr>
2015-03-11 19:43:42 +0100

    Adding global variable example in template

--- template/Main.java
+++ template/Main.java
@@ -3,6 +3,7 @@ import java.io.*;
 import java.math.*;
 
 class Main {
+	public static int uneBonneGrosseVariableGlobale = 3;
 	public static class Point{
 		public int x,y;
 		Point(int n, int v){
@@ -15,6 +16,7 @@ class Main {
 		}
 	}
     public static void main(String args[]) {
-        System.out.println(new Point(42,1337));
+    	uneBonneGrosseVariableGlobale ++;
+        System.out.println(new Point(42,uneBonneGrosseVariableGlobale));
     }
 }
