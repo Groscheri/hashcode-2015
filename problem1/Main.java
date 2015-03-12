502b3afbf1a8cc62d66a9e3f5b49e95a063fc9c9
LeonardA-L <leonard.allain-launay@insa-lyon.fr>
2015-03-12 21:17:36 +0100

    FC

--- problem1/Main.java
+++ problem1/Main.java
@@ -2,18 +2,7 @@ import java.util.*;
 import java.io.*;
 import java.math.*;
 class Main {
-    public static int R, S, U, P, M;
-    public static Map<String,Boolean> unavailable = new HashMap<String,Boolean>();
-    public static ArrayList<Server> servers = new ArrayList<Server>();
-    public static ArrayList<Group> groups = new ArrayList<Group>();
-    
-    public static GameState rootGameState = new GameState();
-    
-    public static class GameState{
-            public ArrayList<Rangee> rangees = new ArrayList<Rangee>();
-            public static ArrayList<Server> servers = new ArrayList<Server>();        // remaining servers
 
-<<<<<<< HEAD
         public static int R, S, U, P, M;
         public static Map<String,Boolean> unavailable = new HashMap<String,Boolean>();
         public static ArrayList<Server> servers = new ArrayList<Server>();
@@ -143,110 +132,20 @@ class Main {
 				this.s = s;
 			}
 		}
-=======
-            // avoir map des scores pour chaque groupe
-            public Map<Group, Integer> getScore(ArrayList<Group> groups) {
-                Map<Group, Integer> scores = new HashMap<Group, Integer>();
-
-                for (Group g : groups) {
-                    scores.put(g, g.getScore());
-                }
-
-                return scores;
-            }
-
-            public void setGroups(ArrayList<Group> groups){
-            	int i,j;
-            	i = 0;
-            	j = servers.size()-1;
-            	Group g;
-            	Server s1;
-            	Server s2;
-            	int numGroup = 0;
-            	g = groups.get(numGroup);
-            	while(i < j){
-            		s1 = servers.get(i);
-            		s2 = servers.get(j);
-            		System.out.println(i+":"+j);
-            		s1.group = g;
-            		s2.group = g;
-            		i++; 
-            		j--;
-            		numGroup++;
-            		if(numGroup == groups.size()){
-              			numGroup = 0;
-            		}
-            		g = groups.get(numGroup);
-            	}
-            	if(i==j){
-            		s1 = servers.get(i);
-            		s1.group = g;
-            	}
-            }
-    }
-    
-    public static class Rangee{
-            int indexOccupied = 0;
-            int index;
-            public Map<Integer, Server> rang = new HashMap<Integer, Server>();
-            public boolean appendServerFF(Server s){
-                    boolean placed = false;
-                    // First fit
-                    Server o;
-                    while((o = rang.get(indexOccupied)) != null){
-                            indexOccupied+=o.z;
-                    }
-                    while(unavailable.get(""+index+"-"+indexOccupied) != null){
-                        indexOccupied++;
-                    }
-                    if(indexOccupied + s.z < S){
-                        rang.put(indexOccupied, s);
-                        s.put(this, indexOccupied);
-                        placed = true;
-                    }
-                    
-                    return placed;
-            }
-            
-            Rangee(int idx){
-                index = idx;
-            }
-    }
-
-    public static class Server{
-        public int z,c;
-        public Rangee r; // rangée où il est placé [null si non placé]
-        public int s; // [slot] emplacement dans la rangée
-        public Group group; //group auquel il appartient
-        public double ratio;
-        Server(int k, int l){
-            r = null;
-            s = -1;
-            z = k;
-            c = l;
-            ratio = (double) c/ (double)z;
-        }
-
-        public void put(Rangee r, int s) {
-            this.r = r;
-            this.s = s;
-        }
-    }
->>>>>>> c16949f17a8627ee45bc9450497768694f347b02
 
 
 	public static class CustomComparator implements Comparator<Server> {
+	    @Override
 	    public int compare(Server o1, Server o2) {
-	        if (o1.ratio > o2.ratio){
+	        if (o1.ratio < o2.ratio){
 	        	return -1;
-	        } else if(o1.ratio < o2.ratio) {
+	        } else if(o1.ratio > o2.ratio) {
 	        	return 1;
 	        }
 	        return 0;
     	}
 	}
 
-<<<<<<< HEAD
     public static class Group implements Serializable{
 		public ArrayList < Server > servers = new ArrayList < Server > ();
 		public int id;
@@ -298,76 +197,13 @@ class Main {
 			return capacity;
 		}
 	}
-=======
-    public static class Group{
-        public ArrayList<Server> servers = new ArrayList<Server>();
-        public int id;
-        Group(int i) {
-        	id = i;
-        }
-        private int capacity_score = 0;
-        private Rangee best = null;
 
-        public int getScore() {
-            Map<Rangee, Integer> scores = new HashMap<Rangee, Integer>();
-            for (Server s : servers) {
-                if (s.r != null) {
-                    // serveur placé
-
-                    Integer current_score = scores.get(s.r);
-                    if (current_score == null ) {
-                        scores.put(s.r, 0);
-                        current_score = 0;
-                    }
-                    scores.put(s.r, current_score + s.z);
-                }
-            }
-
-            //System.out.println("Score du groupe : ");
-            //scores.forEach((k,v) -> System.out.println(k + " => " + v));
-
-            // calculer meilleure rangée
-            scores.forEach((r, i) -> {
-                if (this.best == null) {
-                    this.best = r;
-                }
-                else {
-                    if (i > scores.get(this.best)) {
-                        this.best = r;
-                    }
-                }
-            });
-
-            // calculer score sans elle => score_max - score_meilleure_rangee
-            scores.forEach((r, i) -> {
-                if (r == this.best) {
-                    // nothing to do
-                }
-                else {
-                    this.capacity_score += i;
-                }
-            });
-
-            return this.capacity_score;
-        }
-
-        public int getCapacity() {
-            int capacity = 0;
-            for (Server s : servers) {
-                capacity += s.c;
-            }
-            return capacity;
-        }
-    }
->>>>>>> c16949f17a8627ee45bc9450497768694f347b02
-
-    public static void main(String args[]) throws FileNotFoundException {
+    public static void main(String args[]) {
 
 	//------------------
 
         //----------------- Inputs
-        File file = new File("sample.txt");
-        Scanner in = new Scanner(file);
+        Scanner in = new Scanner(System.in);
         R = in.nextInt();
         S = in.nextInt();
         U = in.nextInt();
@@ -400,13 +236,7 @@ class Main {
                 
         }
 
-        for (int i=0;i<P; i++) {
-            Group g = new Group(i);
-            groups.add(g);
-        }
-
         Collections.sort(rootGameState.servers, new CustomComparator());
-<<<<<<< HEAD
 		rootGameState.setGroups(groups);
 		for (Server server: rootGameState.servers) {
 			System.out.println(server.ratio + " to group : " + server.group.id);
@@ -429,29 +259,6 @@ class Main {
 		}
         
         // --------
-=======
-
-        rootGameState.setGroups(groups);
-
-        for(Server server : rootGameState.servers){
-        	System.out.println(server.ratio+" to group : "+server.group.id);
-        }
-
-        //----------------- Logic
-        
-        //------------------
-
-        groups.get(0).servers.add(servers.get(0));
-        groups.get(0).servers.add(servers.get(1));
-
-        servers.get(0).put(rootGameState.rangees.get(0), 10);
-        servers.get(1).put(rootGameState.rangees.get(0), 30);
-
-        System.out.println("Scores : " + servers.get(0).z + " / " + servers.get(1).z);
-
-        System.out.println("Score final du goupe 0 : " + groups.get(0).getScore());
-
->>>>>>> c16949f17a8627ee45bc9450497768694f347b02
         
         System.exit(0);
 
@@ -514,5 +321,5 @@ class Main {
 		}
 		
 		return o;
-	}
+	} 
 }
